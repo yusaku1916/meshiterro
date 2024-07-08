071b0b72128f791f72d8889949735dc4968a0174
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2024-07-08 03:19:50 +0000

    [Finish]chap15,16

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 
 end
